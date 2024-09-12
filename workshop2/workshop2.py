1c11bca5915872e79483d35ec0e8794a6ac0b5f2
QiuDylan <dylan.qxx@gmail.com>
2024-09-11 23:21:14 -0400

    update

--- workshop2/workshop2.py
+++ workshop2/workshop2.py
@@ -17,24 +17,31 @@ print(tracker_data)
 t = np.genfromtxt("workshop2data.txt", skip_header=1, usecols=0 ) #first column of tracker_data
 x = np.genfromtxt("workshop2data.txt", skip_header=1, usecols=1 ) #second ""
 y = np.genfromtxt("workshop2data.txt", skip_header=1, usecols=2 ) #third ""
-#dt= 0.001 #delt t
+dt= 0.001 #delt t
 
-dydt = np.gradient(y, t)
+#init positions
+x0 = 0
+v0 = 0 
+a0 = 0 
+
+dydt = np.gradient(y, dt)
 d2ydt2 = np.gradient(dydt,t)
 dxdt = np.gradient(x,t)
 d2xdt2 = np.gradient(dxdt,t)
 f, ax = plt.subplots(1)
 #plt.plot(t,x,label='dx/dt')
-plt.plot(x,t,label='x vs t')
-plt.plot(y,t,label='y vs t')
+#plt.plot(t,x,label='x vs t')
+#plt.plot(t,y,label='y vs t')
 #plt.plot(t,y,label='y vs t') #raw plot
 #plt.plot(t,dydt,label= 'dy/dt')
-#plt.plot(t,dxdt,label= 'dx/dt')
+plt.plot(t,dxdt,label= 'dx/dt')
 #plt.plot(t,d2xdt2,label= 'a_x')
 #plt.plot(t,d2ydt2,label= 'a_y')
 #ax.set_ylim(ymin=0)
-#ax.set_xlim(xmin=0)
-plt.xlabel("x(m)")
+plt.xlim(left= 0)
+plt.xlabel("t(s)")
 #plt.ylabel("accel(m/s**2)")
-plt.ylabel("t(s)")
+#plt.ylabel("displacement(m)")
+plt.ylabel("velo(m/s)")
+plt.grid()
 plt.legend()
