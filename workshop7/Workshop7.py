42167ceb16add2026c42678d762f2c0ec71222a9
QiuDylan <dylan.qxx@gmail.com>
2024-10-30 22:44:56 -0400

    update

--- workshop7/Workshop7.py
+++ workshop7/Workshop7.py
@@ -14,12 +14,12 @@ import matplotlib.pyplot as plt
 theta_0 = 10.   #deg, initial angle
 omega_0 = 0.   #rad/sec released from rest
 #INPUT PARAMETERS
-L = 0.9     #m, length of pendulum
+L = 0.90     #m, length of plendulum 
 g = 9.81    #m/s^2, gravity
 #DERIVED PARAMETERS
 theta_0 = theta_0 * np.pi/180   #rad, unit conversion
 tau = 2*np.pi*np.sqrt(L/g)    #s, time constant for small angle pendulum
-
+"""
 #DEFINE FUNCTION FOR EQUATIONS OF MOTION
 def eom(x,t):
     theta, omega = x
@@ -61,15 +61,17 @@ plt.subplot(3,1,3)
 plt.plot(t,T_mg)
 plt.ylabel("T/mg")
 plt.xlabel('time (sec)')  
-
+"""
 #Plendulum
 #Inputs
 l = 1.835 #m
 w = 0.0425 #m
 t = 0.006 #m
 m = 0.226 #kg
+r = 0.26 #m length from com to anchor 
 def density(l,w,t,m):
     density = m / (l*w*t)
+  
     return density
 
 def I_cm(l,w,t,m):
@@ -78,30 +80,72 @@ def I_cm(l,w,t,m):
 
 def I_anchor(l,w,t,m):
     Icm = I_cm(l,w,t,m)
-    i_anchor = Icm + m * (l/2)**2 
+    i_anchor = Icm + m * (r/2)**2 
     return i_anchor
 
-theta = theta_0
-def T_react(l,w,t,m, theta):
-    Ft = m*g*np.sin(theta)
+def get_alpha(l,w,t,m):
+    alpha = -r * m * g * np.sin(theta_0) / I_anchor(l,w,t,m)
+    return alpha
+
+def T_react(l,w,t,m):
+    Ft = m*g*np.sin(theta_0)-m * r * get_alpha(l,w,t,m)
   
     return Ft
-def N_react(l,w,t,m, theta):
-    Fn = m*g*np.cos(theta)
-    print(Fn)
-    return Fn
+def N_react(l,w,t,m):
+    Fn = m*g*np.cos(theta_0)
     
+    return Fn
+
+def eom1(y,t):
+    theta, omega = y
+    ydot = np.zeros(2)
+    ydot[0] = omega
+    ydot[1] = -r * m * g * np.sin(theta)/I_cm(l,w,t,m)
+    return ydot
+#Initial Conditions
+x0 = [theta_0, omega_0]
+
+#Define time array
+time = np.linspace(0, 10*tau, 1000)
+#SOLVE SYSTEM OF EQUATIONS
+sol = odeint(eom1,x0,time)
+
+theta = sol[:,0]            #rad, extract angle
+omega = sol[:,1]            #rad/sec, extract angular speed
+#print(theta)
+#print(omega)
+plt.figure(1)
+ax1 = plt.subplot(211)
+ax1.set_xlim(0,5)
+plt.plot(time,theta*180/np.pi,label='theta (deg)')
+plt.ylabel('Theta (deg)')
+
+ax2= plt.subplot(2,1,2)
+ax2.set_xlim(0,5)
+plt.plot(time,omega, label='omega (rad/s)')
+plt.ylabel('Omega (Revs/sec)')
+
+peak_idx= []   #initialize
+for i in range(len(theta)-2):
+    if theta[i+2] < theta[i+1] and theta[i] < theta[i+1]:
+        peak_idx = np.append(peak_idx,i+1)
+period = time[int(peak_idx[-1])] - time[int(peak_idx[-2])]   
+freq = 1/period
+print('frequency = %.4f Hz' %freq)
+
 def driver():
     density1 = density(l,w,t,m)
     i_cm1 = I_cm(l,w,t,m)
     i_anchor1 = I_anchor(l,w,t,m)
-    Ft = T_react(l,w,t,m,theta)
-    Fn = N_react(l,w,t,m,theta)
+    Ft = T_react(l,w,t,m)
+    Fn = N_react(l,w,t,m)
+    alpha = get_alpha(l,w,t,m)
     print('Density = %.4f kg/m^3' %density1)
     print('I_cm = %.4f kg*m^2' %i_cm1)
     print('I_anchor = %.4f kg*m^2' %i_anchor1)
     print('F_t = %.4f N' %Ft)
     print('F_n = %.4f N' %Fn)
+    print('Alpha = %.4f N' %alpha)
     
 
 
