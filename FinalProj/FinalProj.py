e3b4a06a18a40d81846e5107b66b70bafa3cf8f3
QiuDylan <dylan.qxx@gmail.com>
2024-12-08 16:01:07 -0500

    update

--- FinalProj/FinalProj.py
+++ FinalProj/FinalProj.py
@@ -23,17 +23,32 @@ y = np.genfromtxt("finaldata.txt", skip_header=1, usecols=2 ) #third ""
 """
 
 theta = 24 * np.pi /180 #Windscreen angle in radians
+g = 9.81 #m/s^2
+mu_k = 0.5  #between wiper and windscreen
 
+wiper = 24.5 * 25.4 #link dc mm 
+
+fd = 3.17 * 25.4 #mm
+gf = 12.69 * 25.4 #mm
+og = 2.11 * 25.4 #mm
+mass_wiper = #kg
+
+def link_equations(a_wiper):
+    F_wiper = mass_wiper * a_wiper - mu_k * mass_wiper * g * np.cos(theta)
+    
+    
+
+    return m_torque 
 
 
 fig0 = plt.figure()
 plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace= None, hspace=.75)
 ax0 =fig0.add_subplot(311)
-ax0.plot(t1,x1,label='x1 vs t') #speed set 1
+ax0.plot(t1,x1,label='x1 vs t') #position graph speed set 1
 ax0 =fig0.add_subplot(312)
-ax0.plot(t2,x2,label='x2 vs t') #speed set 2
+ax0.plot(t2,x2,label='x2 vs t') #position graph speed set 2
 ax0 =fig0.add_subplot(313)
-ax0.plot(t2,x2,label='x3 vs t') #speed set 3
+ax0.plot(t2,x2,label='x3 vs t') #position graph speed set 3
 
 
 print()
